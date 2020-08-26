229c78bbeab15fa8189003e81fa49cc09bf6da2a
Abhilash Dorle <47128409+AbhilashDorle@users.noreply.github.com>
2020-08-26 18:08:59 -0400

    Add files via upload

--- recursion.java
+++ recursion.java
@@ -3,7 +3,7 @@
 public int powerN(int base, int n) {
   if(n==1)
   return base;
-  
+
   else
   return base*powerN(base, n-1);
 }
@@ -20,7 +20,7 @@ public int countX(String str) {
     else
       return 0;
   }
-  
+
   if(str.charAt(0)=='x')
     return 1+countX(str.substring(1,str.length()));
   else
@@ -43,7 +43,7 @@ public int countHi(String str) {
 public String changeXY(String str) {
   if(str.length()==0)
     return str;
-  
+
   if(str.charAt(0)=='x')
     return 'y'+changeXY(str.substring(1,str.length()));
   else
@@ -55,7 +55,7 @@ public String changeXY(String str) {
 public String changePi(String str) {
   if(str.length()<2)
     return str;
-  
+
   if(str.substring(0,2).equals("pi"))
     return "3.14"+changePi(str.substring(2));
   else
@@ -67,10 +67,10 @@ public String changePi(String str) {
 public String noX(String str) {
   if(str.length()==0)
     return str;
-  
+
   if(str.charAt(0)=='x')
     return noX(str.substring(1));
-  
+
   return str.charAt(0) + noX(str.substring(1));
 }
 
@@ -87,9 +87,58 @@ public boolean array6(int[] nums, int index) {
     else
       return false;
   }
-  
+
   if(nums[index]==6)
     return true;
-    
+
   return array6(nums,index+1);
+}
+
+//Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+
+public String allStar(String str) {
+  if(str.length()<2)
+    return str;
+
+  return str.charAt(0)+"*"+ allStar(str.substring(1));
+}
+
+//Given a string, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+
+public String pairStar(String str) {
+
+  if(str.length()<2)
+    return str;
+
+  if(str.charAt(0)==str.charAt(1))
+    return str.charAt(0)+"*"+pairStar(str.substring(1));
+  else
+    return str.charAt(0)+pairStar(str.substring(1));
+}
+
+//Given a string, compute recursively a new string where all the lowercase 'x' chars have been moved to the end of the string.
+
+public String endX(String str) {
+  if(str.length()<2)
+    return str;
+
+  if(str.charAt(0)=='x')
+    return endX(str.substring(1)) + str.charAt(0);
+  else
+    return str.charAt(0)+endX(str.substring(1));
+}
+
+//We'll say that a "pair" in a string is two instances of a char separated by a char. So "AxA" the A's make a pair. Pair's can overlap, so "AxAxA" contains 3 pairs -- 2 for A and 1 for x. Recursively compute the number of pairs in the given string.
+
+
+public int countPairs(String str) {
+  if(str.length()<3)
+  {
+    return 0;
+  }
+
+  if(str.charAt(0)==str.charAt(2))
+    return 1+countPairs(str.substring(1));
+  else
+    return 0+countPairs(str.substring(1));
 }
